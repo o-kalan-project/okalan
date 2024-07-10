b40b4fc5b0791eb9e3bbe21330d4c60a5637638c
gachao <134592219+gachao@users.noreply.github.com>
2024-07-10 19:23:31 +0900

    Update e-anitor.js

--- js-library/e-anitor.js
+++ js-library/e-anitor.js
@@ -40,7 +40,7 @@ function _text2(index){
 // _freetext('h1','freetext','id','keifont','30px','pink','middle','popup','1','body');
 async function _freetext(tag,index,id,font,size,color,pos,animation,delay,where){
     var flug=false;
-    conaole.log(document.body.outerWidth)
+    console.log(document.body.outerWidth)
     if(window.outerWidth>=481){
     	var q =document.body.outerWidth/size.slice(0,-2);
     }else{
